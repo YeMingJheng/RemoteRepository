9f08da5972804ce4e06e5b17f9d331ed7079ffdf
葉銘証 <haps@EEIT>
2018-02-03 23:48:16 +0800

    修改Servlet1
    將輸出目前時間改成輸出0~1000的亂數

--- WebLabGit/src/view/Servlet1.java
+++ WebLabGit/src/view/Servlet1.java
@@ -1,20 +1,23 @@
 package view;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.catalina.Session;
 
 public class Servlet1 extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("yayaya");
+		
+		int date = (int)(Math.random()*1000);
+		response.setContentType("text/html , charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Servlet1:"+date+"</h3>");
+		out.close();
 		
 	}
 
