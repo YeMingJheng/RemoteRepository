005b6aae6ac2bac3b03234c56548ba11791e71d3
葉銘証 <haps@EEIT>
2018-02-03 23:33:56 +0800

    修改Servlet1
    期待可以在GitVersionControl/repository內部造成衝突

--- WebLabGit/src/view/Servlet1.java
+++ WebLabGit/src/view/Servlet1.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,11 +11,18 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.catalina.Session;
 
+import com.sun.beans.finder.PrimitiveWrapperMap;
+
 public class Servlet1 extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("yayaya");
+		
+		java.util.Date date = new java.util.Date();
+		response.setContentType("text/html ; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Servlet1執行結果:"+date+"</h3>");
+		out.close();
 		
 	}
 
